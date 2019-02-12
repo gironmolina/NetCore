e1e366d89272a4765aeda72f86d9daf7ac87864d
Huans Hirons <gironmolina@gmail.com>
2019-02-12 22:15:35 +0100

    added gulp

---
+++ NetCoreApp/gulpfile.js
@@ -0,0 +1,12 @@
+﻿var gulp = require("gulp");
+var gnf = require("gulp-npm-files");
+var del = require("del");
+
+gulp.task("clean", function () {
+    return del(["./wwwroot/lib/" + "/**/*"]);
+});
+
+gulp.task("copynpm", function (done) {
+    gulp.src(gnf(), { base: "./" }).pipe(gulp.dest("./wwwroot"));
+    done();
+});
